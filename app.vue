616aebe8b9c6ba4afd6aafc93f2b79aa9fa1693c
DESKTOP-8U2BNO6\Hayato Kojima <majiko2627kojima@gmail.com>
2024-06-24 12:14:23 +0000

    saveeeeee

--- app.vue
+++ app.vue
@@ -3,33 +3,37 @@
 
 <template>
   <div>
-  <header>
-    <h1>私のウェブサイトへようこそ</h1>
-    <nav>
-      <ul>
-        <li><a href="/">ホーム</a></li>
-        <li><a href="/about">アバウト</a></li>
-        <li><a href="/contact">コンタクト</a></li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <section>
-      <h2>最新の記事</h2>
-      <article>
-        <h3>記事タイトル1</h3>
-        <p>この記事の概要...</p>
-        <a href="/post/1">続きを読む</a>
-      </article>
-      <article>
-        <h3>記事タイトル2</h3>
-        <p>この記事の概要...</p>
-        <a href="/post/2">続きを読む</a>
-      </article>
-    </section>
-  </main>
-  <footer>
-      <p>&copy; 2023 私のウェブサイト</p>
+    <header>
+      <h1>
+        <a href="/">
+          Kojima
+        </a>
+      </h1>
+      <nav>
+        <ul>
+          <li><a href="/">ホーム</a></li>
+          <li><a href="/about">アバウト</a></li>
+          <li><a href="/contact">コンタクト</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h2>最新の記事</h2>
+        <article>
+          <h3>記事タイトル1</h3>
+          <p>この記事の概要...</p>
+          <a href="/post/1">続きを読む</a>
+        </article>
+        <article>
+          <h3>記事タイトル2</h3>
+          <p>この記事の概要...</p>
+          <a href="/post/2">続きを読む</a>
+        </article>
+      </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 私のウェブサイト</p>
     </footer>
   </div>
 </template>
